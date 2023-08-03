4ab4048a72613655693ce2bb16cf8fa4accd4e1c
Lucas Fabiano Peres Silva <lucasfafa6@gmail.com-at-851479049298>
2023-08-03 11:56:47 -0300

    Atribuindo atualizacao de dados dos Pacientes

--- src/main/java/med/voll/api/paciente/DadosAtualizacaoPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosAtualizacaoPaciente.java
@@ -1,4 +1,14 @@
 package med.voll.api.paciente;
 
-public record DadosAtualizacaoPaciente(Long id) {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosAtualizacaoPaciente(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        @Valid
+        DadosEndereco endereco) {
 }
